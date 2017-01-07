3933d61644711b4ffeee714adaab5282dc77c7ca
WoKak <michal.woj34@gmail.com>
2017-01-07 14:02:58 +0100

    MyList deleted, another part of an AI implemented

--- src/main/java/logic/ComputerMemory.java
+++ src/main/java/logic/ComputerMemory.java
@@ -6,13 +6,96 @@ package logic;
 public class ComputerMemory {
 
     private Information[] knowledge;
+    private int pointer;
 
     public ComputerMemory() {
 
-        this.knowledge = new Information[64];
+        this.knowledge = new Information[1];
+        this.pointer = 0;
     }
 
     public Information[] getKnowledge() {
+
         return knowledge;
+    }
+
+    public static void myQucikSort(Information table[], int start, int length) {
+
+        int i,j;
+        Information v, tmp;
+
+        i = start;
+        j = length;
+        v = table[(start + length) / 2];
+
+        do {
+
+            while (table[i].compareTo(v) == -1)
+
+                i++;
+
+            while (v.compareTo(table[j]) == -1)
+
+                j--;
+
+            if (i <= j) {
+
+                tmp = table[i];
+                table[i] = table[j];
+                table[j] = tmp;
+                i++;
+                j--;
+            }
+        }
+        while (i <= j);
+
+        if (start < j)
+
+            myQucikSort(table,start,j);
+
+        if (i < length)
+
+            myQucikSort(table,i,length);
+    }
+
+    public void add(Information info) {
+
+        if (pointer == knowledge.length) {
+            Information[] newKnowledge = new Information[knowledge.length + 1];
+            for(int i = 0; i < knowledge.length; i++) {
+                newKnowledge[i] = knowledge[i];
+            }
+            knowledge = newKnowledge;
+        }
+        knowledge[pointer] = info;
+        pointer++;
+    }
+
+    public Information find(Information info) {
+
+        for (int i = 0; i < knowledge.length; i++) {
+
+            if(knowledge[i].equals(info)) {
+
+                return knowledge[i];
+            }
+        }
+
+        return null;
+    }
+
+    public boolean isTwoKnown() {
+
+        this.myQucikSort(this.getKnowledge(), 0, getKnowledge().length - 1);
+
+        for(int i = 1; i < this.getKnowledge().length; i++) {
+
+            if( this.getKnowledge()[i].equals( this.getKnowledge()[i-1]) ) {
+
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- src/main/java/logic/Information.java
+++ src/main/java/logic/Information.java
@@ -3,7 +3,7 @@ package logic;
 /**
  * Created by Michał on 2017-01-06.
  */
-public class Information {
+public class Information implements Comparable<Information> {
 
     private int row;
     private int column;
@@ -37,5 +37,38 @@ public class Information {
 
     public void setNumber(int number) {
         this.number = number;
+    }
+
+    @Override
+    public int compareTo(Information info) {
+
+        if(this.getNumber() > info.getNumber()) {
+
+            return 1;
+
+        } else if (this.getNumber() < info.getNumber()) {
+
+            return -1;
+        }
+
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Information that = (Information) o;
+
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = row;
+        result = 31 * result + column;
+        result = 31 * result + number;
+        return result;
     }
 }
