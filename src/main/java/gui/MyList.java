6520a8fefe5aaedea21f4a5c590328cabdc8b64f
pawel <pawel@pawel-Latitude-E6510>
2017-01-16 11:59:41 +0100

    MyList instead of ArrayList

---
+++ src/main/java/gui/MyList.java
@@ -0,0 +1,20 @@
+
+package gui;
+
+/**
+ *
+ * @author pawel
+ */
+public class MyList {
+    
+    int[] DynamicArray;
+    int size;
+    
+    public MyList () {
+        
+    }
+           
+    
+    
+    
+}
